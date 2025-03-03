c8e3f417a049d748e3e9cfe6a9929df6da3a5433
Bheema Rohith <110445166+2100031331@users.noreply.github.com>
2025-03-03 14:49:32 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://online-food-delivery-webileapps-project.onrender.com";
   const [token, setToken] = useState(localStorage.getItem("token") || "");
   const [userName, setUserName] = useState(localStorage.getItem("userName") || ""); // Store username
   const [food_list, setFoodList] = useState([]);
