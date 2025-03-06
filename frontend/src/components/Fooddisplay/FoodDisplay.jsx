1871c78ffe8df8935226c3715584d1bbbfdff387
Bheema Rohith <110445166+2100031331@users.noreply.github.com>
2025-03-06 16:28:54 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/Fooddisplay/FoodDisplay.jsx
+++ frontend/src/components/Fooddisplay/FoodDisplay.jsx
@@ -4,13 +4,14 @@ import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
 const FoodDisplay = ({ category }) => {
-  const { food_list } = useContext(StoreContext);
+  const { filteredFoodList } = useContext(StoreContext); // Use filtered food list
+
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        {food_list.map((item, index) => {
-          if (category==="All" || category===item.category) {
+        {filteredFoodList.map((item, index) => {
+          if (category === "All" || category === item.category) {
             return (
               <FoodItem
                 key={index}
@@ -22,6 +23,7 @@ const FoodDisplay = ({ category }) => {
               />
             );
           }
+          return null;
         })}
       </div>
     </div>
